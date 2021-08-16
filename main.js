c6ba4e5dcc36c8ebc5138af0a61476e120c1d3d7
Lesley_Heyer <lesley@wewhistle.com>
2021-08-16 16:48:38 -0500

    finished lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'These are great socks'
+        }
+    }
+})
+//you have to pass in an object (even if its empty) to the createApp
